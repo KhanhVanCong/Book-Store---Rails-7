bfc6501b21e311f753db1b519ae56e2ec25325ea
khanhvancong <khanhvc2310@gmail.com>
2022-09-07 16:57:51 +0700

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, module: "users"
+
+  as :user do
+    get "login" => "users/sessions#new"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
