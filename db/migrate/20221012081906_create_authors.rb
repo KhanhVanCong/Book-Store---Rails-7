31a7129ea0c10e86d752518c2f7b689c9da85ecc
khanhvancong <khanhvc2310@gmail.com>
2022-10-12 16:41:03 +0700

    Init model which relatives of books
    Add books, authors

---
+++ db/migrate/20221012081906_create_authors.rb
@@ -0,0 +1,15 @@
+class CreateAuthors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :authors do |t|
+      t.string :name
+      t.string :description
+      t.string :facebook_link
+      t.string :instagram_link
+      t.string :google_plus_link
+      t.string :twitter_link
+      t.integer :books_count, default: 0
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/books.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :book do
+    title { "MyString" }
+    description { "MyString" }
+    price { "MyString" }
+    is_featured { "MyString" }
+  end
+end
